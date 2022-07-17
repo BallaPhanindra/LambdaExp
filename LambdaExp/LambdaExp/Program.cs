4a4daf452f310e684928bc938c3e7597d091f03a
Balla <ballaphanindra@outlook.com>
2022-07-18 01:03:26 +0530

    updating User Reg

--- LambdaExp/LambdaExp/Program.cs
+++ LambdaExp/LambdaExp/Program.cs
@@ -10,9 +10,9 @@ namespace LambdaExp
         {
             List<Person> listPersonsInCity = new List<Person>();
             AddRecords(listPersonsInCity);
-            foreach(Person persons in listPersonsInCity)
+            foreach (Person persons in listPersonsInCity)
             {
-                Console.WriteLine(persons.Name+" "+persons.Address+" "+persons.Age+" "+persons.SSN);
+                Console.WriteLine(persons.Name + " " + persons.Address + " " + persons.Age + " " + persons.SSN);
             }
             Console.WriteLine("===============================");
             RetrievingTopTwoRecordsForAgeIsLessThanSixty(listPersonsInCity);
@@ -21,27 +21,29 @@ namespace LambdaExp
             Console.WriteLine("===============================");
             RetrieveAverageAge(listPersonsInCity);
             Console.WriteLine("===============================");
+      
+            Console.WriteLine("===============================");
         }
         public static void AddRecords(List<Person> listPersonsInCity)
         {
-    listPersonsInCity.Add(new Person("234345456", "Balla", "H.No. 9-99/9,Sardar Road,Houston", 15));
-    listPersonsInCity.Add(new Person("245342234", "Rahul", "H.No. 8-11/9,Subhash Road,Houston", 26));
-    listPersonsInCity.Add(new Person("457653456", "Sachin", "H.No. 2-9,BhagathSingh Road,Houston", 13));
-    listPersonsInCity.Add(new Person("905689878", "Virendar", "H.No. 1/9,Udham Road,Houston", 67));
-    listPersonsInCity.Add(new Person("342121234", "Sourabh", "H.No. 3/9-1,Alluri Road,Houston", 74));
-    listPersonsInCity.Add(new Person("789098765", "VVS", "H.No. 12/2-1,Komaram Bhim Road,Houston", 61));
-    listPersonsInCity.Add(new Person("642725421", "Yuvaraj", "H.No. 123/2,Shoppers Road,Houston", 26));
+            listPersonsInCity.Add(new Person("234345456", "Balla", "H.No. 9-99/9,Sardar Road,Houston", 15));
+            listPersonsInCity.Add(new Person("245342234", "Rahul", "H.No. 8-11/9,Subhash Road,Houston", 26));
+            listPersonsInCity.Add(new Person("457653456", "Sachin", "H.No. 2-9,BhagathSingh Road,Houston", 13));
+            listPersonsInCity.Add(new Person("905689878", "Virendar", "H.No. 1/9,Udham Road,Houston", 67));
+            listPersonsInCity.Add(new Person("342121234", "Sourabh", "H.No. 3/9-1,Alluri Road,Houston", 74));
+            listPersonsInCity.Add(new Person("789098765", "VVS", "H.No. 12/2-1,Komaram Bhim Road,Houston", 61));
+            listPersonsInCity.Add(new Person("642725421", "Yuvaraj", "H.No. 123/2,Shoppers Road,Houston", 26));
         }
         public static void RetrievingTopTwoRecordsForAgeIsLessThanSixty(List<Person> listPersonsInCity)
         {
-        foreach (Person person in listPersonsInCity.FindAll(e=>(e.Age>=60)).Take(2).ToList())
+            foreach (Person person in listPersonsInCity.FindAll(e => (e.Age >= 60)).Take(2).ToList())
             {
-            Console.WriteLine("Name "+person.Name+" \t\tAge: "+person.Age);
+                Console.WriteLine("Name " + person.Name + " \t\tAge: " + person.Age);
             }
         }
         public static void CheckingForTeenagerPerson(List<Person> listPersonsInCity)
         {
-            if(listPersonsInCity.Any(e=>(e.Age<=19&&e.Age>=13)))
+            if (listPersonsInCity.Any(e => (e.Age <= 19 && e.Age >= 13)))
             {
                 Console.WriteLine("Yes, We have some teenagers in the list");
             }
@@ -55,6 +57,19 @@ namespace LambdaExp
             double average = list.Average(p => p.Age);
             Console.WriteLine(average);
         }
-
+        public static void CheckValidFirstName(List<Person> listPersonsInCity)
+        {
+            
+            Console.WriteLine();
+        }
+        public static Func<Person, bool> ValidFirstName = (p) =>
+        {
+            if (p.Name.Length < 3)
+            {
+                return false;
+            }
+            else
+                return true;
+        };
     }
 }
